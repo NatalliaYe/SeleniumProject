ad5840ff85b9e55e0cfb61978e9285232b5ce51b
Natalya <natalyhubchuk@gmail.com>
2025-10-06 17:26:12 +0300

    Added  next homework- created 3 test methods for clicking links in the header;  other tests with actions

--- src/test/java/SeleniumTestOne.java
+++ src/test/java/SeleniumTestOne.java
@@ -1,25 +1,85 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Duration;
+import java.util.Objects;
 
+@Test
 public class SeleniumTestOne {
 
-    @Test
+    public void actionsTestOne(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://litecart.stqa.ru/en/");
+        WebElement homeButton = driver.findElement(By.className("fa-home"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(homeButton).click(homeButton).perform();
+        String pageTitle = driver.getTitle();
+        Assert.assertTrue(Objects.equals(pageTitle, "Online Store | My Store"));
+
+    }
+
+    public void actionsTestTwo() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://litecart.stqa.ru/en/");
+        WebElement headerLink = driver.findElement(By.cssSelector("[class='category-1']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(headerLink).click(headerLink).perform();
+        String pageTitle = driver.getTitle();
+        Assert.assertEquals(pageTitle, "Rubber Ducks | My Store");
+    }
+
+
+    public void actionsTestThree() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://litecart.stqa.ru/en/");
+        WebElement headerLink = driver.findElement(By.linkText("Rubber Ducks"));
+        WebElement subHeaderLink = driver.findElement(By.cssSelector("[class='category-2']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(headerLink).click(subHeaderLink).perform();
+        String pageTitle = driver.getTitle();
+        Assert.assertEquals(pageTitle, "Subcategory | My Store");
+
+    }
+
+    public void differentActionsTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://litecart.stqa.ru/en/");
+        WebElement redDuck = driver.findElement(By.cssSelector("img[alt='Red Duck']"));
+        WebElement logoButton = driver.findElement(By.cssSelector("img[alt='My Store']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(redDuck).click(redDuck).moveByOffset(300,0).
+                moveByOffset(0,-300).perform();
+
+    }
+    public void nextActionsTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://litecart.stqa.ru/en/");
+
+        WebElement redDuck = driver.findElement(By.cssSelector("img[alt='Red Duck']"));
+        Actions actions = new Actions(driver);
+        actions.clickAndHold(redDuck).release().contextClick().perform();
+
+    }
+
+
+
     public void seleniumFirstTest() {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com");
-
-        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
-        Assert.assertEquals(allLinks.size(), 46);
-
-        allLinks.get(10).click();
-
+//        driver.get("https://the-internet.herokuapp.com");
+//
+//        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+//        Assert.assertEquals(allLinks.size(), 46);
+//
+//        allLinks.get(10).click();
 
     }
 
